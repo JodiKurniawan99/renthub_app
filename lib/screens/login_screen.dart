9c1ec0f5ad7101d7fc91b8a7bd5e6e266ba8a6fd
JodiKurniawan99 <49111430+JodiKurniawan99@users.noreply.github.com>
2021-12-08 01:38:29 +0700

    init splash screen and login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const String routeId = 'login_screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+class _LoginScreenState extends State<LoginScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Email',
+              ),
+            ),
+            SizedBox(height: 8.0),
+            TextField(
+
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.visibility_off),
+                  onPressed: () {
+                  },
+                ),
+                hintText: 'Password',
+              ),
+            ),
+            SizedBox(height: 24.0),
+            MaterialButton(
+              child: Text('Login'),
+              color: Theme.of(context).primaryColor,
+              textTheme: ButtonTextTheme.primary,
+              height: 40,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              onPressed: () {}
+            ),
+            TextButton(
+              child: Text('Belum punya akun? register disini'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:renthub_app/screens/login_screen.dart';
+
+class SplashScreen extends StatefulWidget{
+  const SplashScreen({Key? key}) : super(key: key);
+  static const routeId = '/splash_screen';
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController controller = AnimationController(
+    duration: const Duration(milliseconds: 2000),
+    vsync: this,
+  )..repeat(reverse: false);
+  late final Animation<Offset> _offsetAnimation = Tween<Offset>(
+    begin: Offset.zero,
+    end: const Offset(3.0, 0.0),
+  ).animate(CurvedAnimation(
+    parent: controller,
+    curve: Curves.elasticIn,
+  ));
+
+  launchApp() async {
+    var time = const Duration(milliseconds: 2300);
+    return Timer(time, toHomeScreen);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    launchApp();
+  }
+
+  void toHomeScreen() {
+    Navigator.pushReplacementNamed(context, LoginScreen.routeId);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xFF0d0b33),
+        ),
+        child: Center(
+            child: SlideTransition(
+          position: _offsetAnimation,
+          child: Icon(
+            Icons.fastfood_rounded,
+            color: Colors.white,
+            size: 60,
+          ),
+        )),
+      ),
+    );
+  }
+}
