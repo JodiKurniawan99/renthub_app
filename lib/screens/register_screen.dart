5e0afb5788a1c9fa21d44c9cf2ad64358b06c64f
nurulrhy <nurulrahayu027@gmail.com>
2021-12-10 01:23:37 +0700

    Fix Register Screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -11,86 +11,88 @@ class _RegisterScreen extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(),
-              SizedBox(height: 100.0),
-              Text('REGISTRATION',
-                  textAlign: TextAlign.center,
-                  style:
-                      TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold)),
-              SizedBox(height: 50.0),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.person),
-                  hintText: 'Name',
-                ),
-              ),
-              SizedBox(height: 8.0),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.alternate_email),
-                  hintText: 'Username',
-                ),
-              ),
-              SizedBox(height: 8.0),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.lock),
-                  hintText: 'Password',
-                ),
-              ),
-              SizedBox(height: 8.0),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.phone),
-                  hintText: 'Phone Number',
-                ),
-              ),
-              SizedBox(height: 8.0),
-              TextFormField(
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.home),
-                  hintText: 'Address',
-                ),
-              ),
-              SizedBox(height: 24.0),
-              MaterialButton(
-                  child: Text('Register'),
-                  color: Theme.of(context).primaryColor,
-                  textTheme: ButtonTextTheme.primary,
-                  height: 40,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(),
+                SizedBox(height: 100.0),
+                Text('REGISTRATION',
+                    textAlign: TextAlign.center,
+                    style:
+                        TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold)),
+                SizedBox(height: 50.0),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                    hintText: 'Name',
                   ),
+                ),
+                SizedBox(height: 8.0),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.alternate_email),
+                    hintText: 'Username',
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock),
+                    hintText: 'Password',
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.phone),
+                    hintText: 'Phone Number',
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                TextFormField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.home),
+                    hintText: 'Address',
+                  ),
+                ),
+                SizedBox(height: 24.0),
+                MaterialButton(
+                    child: Text('Register'),
+                    color: Theme.of(context).primaryColor,
+                    textTheme: ButtonTextTheme.primary,
+                    height: 40,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => HomeCustcreen()));
+                    }),
+                TextButton(
+                  child: Text('Already Registered? Login'),
                   onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => HomeCustcreen()));
-                  }),
-              TextButton(
-                child: Text('Already Registered? Login'),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => LoginScreen()));
-                },
-              ),
-            ],
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()));
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
